c7349ead96b50761589145d8b1f77b372014952f
jocular-source-7570 <mjtvan@gmail.com>
2019-08-18 14:02:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,28 @@
 # require modules here
-
-def load_library
-  # code goes here
+require "yaml"
+def load_library(path)
+  emoticons = YAML.load_file(path)
+  hash = {
+  "get_meaning" => {},
+  "get_emoticon" => {}
+  }
+  emoticons.each{|meaning, emoji|
+  hash["get_meaning"][emoji[1]] = meaning
+  hash["get_emoticon"][emoji[0]] = emoji[1]
+  }
+  return hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, emoticon_input)
+  emoticons = load_library(path)
+ if emoticons["get_emoticon"].include?(emoticon_input)
+  emoticons["get_emoticon"][emoticon_input]
+else 
+ return  "Sorry, that emoticon was not found"
+end 
+
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(path, emoticon_input)
+  library = load_library(path)
 end
